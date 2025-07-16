66bc78d8f4d59fa0acc3aa9b63f8fa0635a68574
Ameb8 <103926493+Ameb8@users.noreply.github.com>
2025-07-16 12:20:08 -0700

    Update cpy.rb resources block

--- Formula/cpy.rb
+++ Formula/cpy.rb
@@ -10,21 +10,19 @@ class Cpy < Formula
   depends_on "python@3.11"
 
   resource "pyperclip" do
-    
-    url "https://github.com/Ameb8/cpy/releases/download/v1.1.1/cpy-1.1.1.tar.gz"
-    sha256 "bb78d69b9812880e078494a66fb1efa7fd18a4ba3f67e4a4496d28c7e83f7585"
+    url "https://files.pythonhosted.org/packages/ea/5d/54e0df42141cefb819d9e84763c49fdb6fa4b3cc53a3cc2e4e5e1f6898d8/pyperclip-1.9.0.tar.gz"
+    sha256 "65d7c037fbc8c24c678badfd6fe0a4d9ff80c24fb25be1d0f02a53990c72b31b"
   end
-
+  
   resource "treelib" do
-    url "https://github.com/Ameb8/cpy/releases/download/v1.1.1/cpy-1.1.1.tar.gz"
-    sha256 "bb78d69b9812880e078494a66fb1efa7fd18a4ba3f67e4a4496d28c7e83f7585"
+    url "https://files.pythonhosted.org/packages/46/7f/9335c1b48f5dc1dd8dd3d1e009ddf11ea55b9a24b9fc4d22d5cf7e547f2e/treelib-1.8.0.tar.gz"
+    sha256 "ab8ac5b2679fc7c1f740a1720e317c7d1d9a64824c0933eaaae3c49dcbb89b8e"
   end
-
+  
   resource "six" do
-  url "https://github.com/Ameb8/cpy/releases/download/v1.1.1/cpy-1.1.1.tar.gz"
-  sha256 "bb78d69b9812880e078494a66fb1efa7fd18a4ba3f67e4a4496d28c7e83f7585"
-end
-
+    url "https://files.pythonhosted.org/packages/02/75/9284b89f7be4ba769ae8e4e50a1f30f84cb22b3ae391d0cc0491e13f2b69/six-1.16.0.tar.gz"
+    sha256 "1e61c37477a1626458e36f7b1d82aa5c0f5c6ac6f6cbee8520f546f3cb274518"
+  end
 
   def install
     virtualenv_install_with_resources
